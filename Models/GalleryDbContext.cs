0faf33c186f1afe412be19787074f9a1af00e365
Diego C. <delcaos12@gmail.com>
2026-04-07 07:39:38 -0500

    Detalles de string

--- Models/GalleryDbContext.cs
+++ Models/GalleryDbContext.cs
@@ -19,9 +19,7 @@ public partial class GalleryDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=127.0.0.1,1444;Database=GalleryDB;User Id=sa;Password=Contrasenamuysegura+123;TrustServerCertificate=True;");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
